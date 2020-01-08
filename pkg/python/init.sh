f4376fe6924ed65a83d9ddf67400b780d12000c1
rachidbch <rachidbch@gmail.com>
2020-01-08 10:44:52 -0800

    Added pyhon3.6 install for Ubuntu <= 16.04 in python pkg

--- pkg/python/init.sh
+++ pkg/python/init.sh
@@ -1,8 +1,55 @@
+UBUNTU_VER="$(lsb_release -r | cut -f2)"
 
 # Python2 should always be accessible through `python` command
-if [ -z "$(command -v "python" 2>&1)"  ]; then sudo apt-get -q install python -y ; fi
-# Maybe we don't have python3
-if [ -z "$(command -v "python3" 2>&1)"  ]; then sudo apt-get -q install python3 -y; fi
+if [ -z "$(command -v "python" 2>&1)"  ]; then 
+  [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -60)" ] &&  sudo apt-get -q update
+  sudo apt-get -q install python -y 
+fi
+
+## Python3 should always be accessible through `python` command
+if [ -z "$(command -v "python3" 2>&1)"  ]; then 
+  [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -60)" ] &&  sudo apt-get -q update
+  sudo apt-get -q install python3 -y 
+fi
+
+# On Ubuntu 16.04 official apt repo doesn't have Python 3.5+
+# A lot of google pages point to adding jonathonf/python-3.6 but but jonathon has unpublished a lot of its packages
+# The alternative is deadsnakes (see https://askubuntu.com/a/865569)
+# =warning= 
+
+# For unknown reason add-apt below failed with an error stating that the public KEY wasn't found
+# I had to manually:
+# 1. Go to https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa
+# 2. Find the Signing key fingerprint of the ppa  
+# 3. Import the key with
+#    $ gpg --keyserver keyserver.ubuntu.com --recv-keys F23C5A6CF475977595C89F51BA6932366A755776
+# 5. Add the public the key to Ubunut apt trusted keys database
+#    $ gpg --export --armor  F23C5A6CF475977595C89F51BA6932366A755776 | sudo apt-key add -
+# 6. And then only proceed to add the apt repository and apt update
+if (( $(echo "$UBUNTU_VER <= 16.04" | bc -l ))); then
+  if [ -z "$(command -v "python3.6" 2>&1)"  ]; then 
+    echo "1 ==="
+    if ! grep -q "^deb .*deadsnakes/ppa" /etc/apt/sources.list.d/*; then
+      echo "2 ==="
+      sudo add-apt-repository ppa:deadsnakes/ppa 
+      sudo apt-get -q update
+    fi
+    echo "3 ==="
+    sudo apt-get install python3.6 
+
+    # Set python3.6 as the default python3
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+    # =warning= If this doesn't do the job, select manually the alternative with:
+    # $ sudo update-alternatives --config python3
+
+    # Install pip3.6 as it python3.6 pkg comes without it
+    curl https://bootstrap.pypa.io/get-pip.py | sudo python3.6
+
+  fi
+fi
+
+
 
 # =todo= The following package install fails on 16.04.06 LTS. As it was a workaround for the problem below, try to found out what to do with it!
 # without this, following pip3 install [[https://github.com/pypa/pip/issues/5367#issuecomment-387354705][fails]]

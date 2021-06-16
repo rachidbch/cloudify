ac4a222380858055390378a73c97588268712d13
rachidbch <rachidbch@gmail.com>
2021-06-16 14:38:24 +0000

    hub and gh packages have the same init script. This script should be extracted in its own package (previous commit message was wrong)

--- pkg/basics/init.sh
+++ pkg/basics/init.sh
@@ -6,11 +6,13 @@ sudo apt-get -q install language-pack-en -y
 # Without software-propreties-common this no add-apt-repository ...
 # Add a mini comment to explain other installs
 sudo apt-get -q install apt-transport-https ca-certificates curl gnupg-agent -y
-software-properties-common bc tree -y
+software-properties-common bc -y
 
 # trash-cli is used by workify to safely delete files and directories 
 sudo apt-get -q install trash-cli -y
 
 # Nice to have tooling 
-sudo apt-get -q install tree -y
+sudo apt-get -q install tree psproc -y
+sudo apt-get -q install jq -y
+sudo apt-get -q install rename -y
 

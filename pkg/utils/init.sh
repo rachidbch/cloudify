42f62f5dcdbb6cd8db71ad9ac0618adf459ecdf3
rachidbch <rachidbch@gmail.com>
2021-06-18 20:06:50 +0000

    Fixed how workify sets local bin in the path. Renamed 'interactivepath' to shorter 'ipath'

--- pkg/utils/init.sh
+++ pkg/utils/init.sh
@@ -2,19 +2,20 @@
 #
 # Utils pkg gathers small in-house utility scripts
 
-# ~/.local/bin/interactivepath allows you to edit your PATH in VIM
+# ~/.local/bin/ipath allows you to edit your PATH in VIM
 # =NOTE= As it must manipulate your shell environment, you must source it and not execute it.
 
-[[ -e ~/.local/bin/interactivepath ]] && rm ~/.local/bin/interactivepath
-cat -> "$HOME"/.local/bin/interactivepath <<- EOF
+[[ -e ~/.local/bin/ipath ]] && rm ~/.local/bin/ipath
+cat -> "$HOME"/.local/bin/ipath <<- EOF
 	PATHBACK=\$PATH
 	PATHNEW=\$( echo \$PATH | tr ':' '\n' | EDITOR=vi vipe | sed '/^$/d' | tr '\n' ':')
-
-	# Check everything is OK
+	# Remove last ':'
+	PATHNEW=\$(sed -r 's/:\$//' <<<"\$PATHNEW")
+	# echo Check everything is OK
 	# echo \$PATHNEW | tr ':' '\n' 
 	# Replace
 	export PATH=\$PATHNEW && unset PATHNEW && unset PATHBACK 
 EOF
 
-# interactivepath must be sourced not executed, otherwise it will have no effect on your shell environment
-chmod -x "$HOME"/.local/bin/interactivepath
+# ipath must be sourced not executed, otherwise it will have no effect on your shell environment
+chmod -x "$HOME"/.local/bin/ipath

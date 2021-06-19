55b3761228caada00321915988f18d8b8cded851
rachidbch <rachidbch@gmail.com>
2021-06-19 14:12:54 +0000

    Workify can be given multiple package to install. Basics package installs base-only 'en' language pack

--- pkg/basics/init.sh
+++ pkg/basics/init.sh
@@ -1,7 +1,7 @@
 # Some stations come without proper locales
 # On RackNerd VPS, I had a lot of " perl: warning: Setting locale failed." errors
 # Some advise to be selective and install only needed locales. How?
-WRKFY_APT_INSTALL language-pack-en 
+WRKFY_APT_INSTALL language-pack-en-base
 
 # Without software-propreties-common this no add-apt-repository ...
 # Add a mini comment to explain other installs

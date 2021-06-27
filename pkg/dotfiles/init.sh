daa8121e42950211dc73157e29cb089e24ebc237
rachidbch <rachidbch@gmail.com>
2021-06-27 17:48:07 +0000

    Added password-less git operation using PAT (Personal Access Token) (still broken)
    * cloudify/cloudify
    ** Added a git function
       git function shadows the system git command
       When 'git' command is called from a package's init.sh file, cloudify's git execute to ensure git authentication before calling system git
       This is needed to have password-less git commands (just like password-less sudo commands)
       That way package init scritps are run against a remote host
    ** Added pkg_break function
       Break the execution and waits for ENTER key
       Just helps in debugging
    ** Fixed emptydir function
    ** msg function can now take multiple arguments
    ** fixed pkg_backup
       Reliably move stuff in /tmp/cloudify
       Maintains a backup index to know how many times a files has been backed up
       This is usefull to clean up things when the program exit
       But has a lot of dummy echo for dummy debuging
       =todo= remove dummy echo lines
    ** fixed pkg_restore
       Reliably restores stuff from /tmp/cloudify
       Maintains a backup index to know how many times a files has been backed up
       This is usefull to clean up things when the program exit
    ** pkg/play
       Added a dummy package for testing purpose

--- pkg/dotfiles/init.sh
+++ pkg/dotfiles/init.sh
@@ -2,44 +2,24 @@
 
 # To simplify for now we don't use XDG_CONGIG dir. Use it? 
 #[ -d ~/.config/dotfiles ] || git clone https://gitlab.com/mobilefirstcentury/dotfiles.git ${XDG_CONFIG_HOME:-~/.config}/dotfiles
-
+echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa
 pkg_depends stow 
 
 # Clone cloudfiles repo
 if [ -d "$HOME"/.dotfiles ]; then
-  PKG_DEBUG_LN "Updating dotfiles"
+  PKG_DEBUG 'Updating dotfiles'
   ( cd "$HOME"/.dotfiles;   
-    # Decide if the remote is on github or gitlab
-    local git_origin=$(git remote get-url origin)
-    #origin_with_pass=${origin/"//"/"//${USER_ID}:${USER_PW}@"}
-    #git pull ${origin_with_pass} master 
-
-    local is_gitlab=$( echo "$git_origin" | grep -Fq gitlab.com )
-    local is_github=$( echo "$git_origin" | grep -Fq github.com )
-
-    if "$is_gitlab"; then
-      git pull --recurse-submodules <<< "$CLOUDIFY_GITLABUSER\n$CLOUDIFY_GITLABPWD"
-      git submodule update --recursive --remote  <<< "$CLOUDIFY_GITLABUSER\n$CLOUDIFY_GITLABPWD"
-    elif "$is_github"; then
-      git pull --recurse-submodules <<< "$CLOUDIFY_GITHUBUSER\n$CLOUDIFY_GITHUBPWD"
-      git submodule update --recursive --remote <<< "$CLOUDIFY_GITHUBUSER\n$CLOUDIFY_GITHUBPWD"
-    else
-      die "Remote git of package $(git remote show origin | grep -F Fetch | cut  -d' ' -f5) not supported" 1
-    fi
+      echo "just before git pull"
+      #git pull --recurse-submodules 
+      git pull 
+      echo "just after git pull"
+      git submodule update --recursive --remote  
   )
 else
   PKG_DEBUG_LN "Downloading dotfiles"
   git clone --recurse-submodules https://gitlab.com/mobilefirstcentury/dotfiles.git  "$HOME"/.dotfiles
   ( cd "$HOME"/.dotfiles; git submodule update --recursive --remote )
 fi
-
-PKG_DEBUG "Backuping up .bashrc"
-[[ -d "$CLOUDIFY_TMP"/backup ]] || mkdir "$CLOUDIFY_TMP"/backup 
-mv "$CLOUDIFY_TMP"/backup/.bashrc.bak.4 "$CLOUDIFY_TMP"/backup/.bashrc.bak.5 2>/dev/null 
-mv "$CLOUDIFY_TMP"/backup/.bashrc.bak.3 "$CLOUDIFY_TMP"/backup/.bashrc.bak.4 2>/dev/null
-mv "$CLOUDIFY_TMP"/backup/.bashrc.bak.2 "$CLOUDIFY_TMP"/backup/.bashrc.bak.3 2>/dev/null
-mv "$CLOUDIFY_TMP"/backup/.bashrc.bak  "$CLOUDIFY_TMP"/backup/.bashrc.bak.2 2>/dev/null
-mv "$CLOUDIFY_TMP"/bashrc "$CLOUDIFY_TMP"/backup/.bashrc.bak
 
 PKG_DEBUG "Installing stowit"
 ln -sfn "$HOME"/.dotfiles/stow/stowit "$HOME"/.local/bin/stowit
---
+++ pkg/play/init.sh
@@ -0,0 +1,1 @@
+git clone "https://github.com/rachidbch/play.git" /tmp/cloudify/play

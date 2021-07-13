8f09042ebdf5595ae90220e086c858b7e09b5f45
rachidbch <rachidbch@gmail.com>
2021-07-13 01:44:05 +0100

    Adding docker cloudify package. Fixing sudo add-apt-repository

--- pkg/docker/init.sh
+++ pkg/docker/init.sh
@@ -8,10 +8,7 @@ sudo apt-get -q install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get -q update -y
 

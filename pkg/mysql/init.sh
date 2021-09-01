5582467af7ab7a9edec7488dcade32c581fdc6cb
rachidbch <rachidbch@gmail.com>
2021-09-01 19:06:59 +0100

    Fixing rclone package

--- pkg/mysql/init.sh
+++ pkg/mysql/init.sh
@@ -4,6 +4,6 @@
 # download bat deb in ~/workstation/install/deb
 curl -LSs "https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb" > mysql.deb
 
-# install bat
+# install mysql deb
 sudo apt-get -q update
 sudo apt-get -q install ./mysql.deb -y
--- pkg/rclone/init.sh
+++ pkg/rclone/init.sh
@@ -1,24 +1,26 @@
-RClone
-======
+# Rclone Installation 
 
-# notes: release links
-#    v1.43.1  Linux 64 : https://downloads.rclone.org/v1.43.1/rclone-v1.43.1-linux-amd64.deb
+pkg_install_release rclone "rclone/rclone"
 
-# download rclone deb in ~/workstation/install/deb
-curl -LSs "https://downloads.rclone.org/v1.43.1/rclone-v1.43.1-linux-amd64.deb" > rclone.deb
 
-# install rclone
-sudo apt-get -q update
-sudo apt-get -q install ./rclone.deb
-
-# install megadown
-source "$WORKSTATION_DIR"/installs/megadown/install.sh
-
-# to bootstrap the installation you need rclone passwords saved in  mega:BOOTSTRAP/rclone.conf
-# fortunately mega can generate direct file urls but we need megadown script to downaload in a script (or on command line)
-# download rclone.conf
-if [ ! -d "~/.config/rclone"  ]; then mkdir -p ~/.config/rclone; fi
-megadown 'https://mega.nz/#!5FwRwA6a!V2Z4JUDPuQ_C0INzyLO4hzdfrvFIx-51mo7AzepPGng' -o ~/.config/rclone/rclone.conf
-
-# sets up autompletion for bash shell
-sudo rclone genautocomplete bash
+## notes: release links
+##    v1.43.1  Linux 64 : https://downloads.rclone.org/v1.43.1/rclone-v1.43.1-linux-amd64.deb
+#
+## download rclone deb in ~/workstation/install/deb
+#curl -LSs "https://downloads.rclone.org/v1.43.1/rclone-v1.43.1-linux-amd64.deb" > rclone.deb
+#
+## install rclone
+#sudo apt-get -q update
+#sudo apt-get -q install ./rclone.deb
+#
+## install megadown
+#source "$WORKSTATION_DIR"/installs/megadown/install.sh
+#
+## to bootstrap the installation you need rclone passwords saved in  mega:BOOTSTRAP/rclone.conf
+## fortunately mega can generate direct file urls but we need megadown script to downaload in a script (or on command line)
+## download rclone.conf
+#if [ ! -d "~/.config/rclone"  ]; then mkdir -p ~/.config/rclone; fi
+#megadown 'https://mega.nz/#!5FwRwA6a!V2Z4JUDPuQ_C0INzyLO4hzdfrvFIx-51mo7AzepPGng' -o ~/.config/rclone/rclone.conf
+#
+## sets up autompletion for bash shell
+#sudo rclone genautocomplete bash

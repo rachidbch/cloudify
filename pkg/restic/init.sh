34668c434a613879812d8aef8b961c84621439ef
rachidbch <rachidbch@gmail.com>
2021-09-04 11:20:28 +0100

    Fixing restic package. Name of daily cron backup script can only have '-' & '_' in name

--- pkg/restic/init.sh
+++ pkg/restic/init.sh
@@ -23,18 +23,18 @@ PKG_DEBUG "Created 'Default' restic backup Operation"
 
 # =NOTE= Look at the dance around the simple quote!
 #        Unfortunately this is required because the whole heredoc will be surrounded by single quote in our implementation of tee shadow function
-#        And this escape syntax (The only way to escape a single quote inside a single quote string) is needed for the quote to make it to the cloudify.backup file ...
+#        And this escape syntax (The only way to escape a single quote inside a single quote string) is needed for the quote to make it to the cloudify-backup file ...
 PKG_DEBUG "Creating Default Backup cron job"
-sudo tee /etc/cron.daily/cloudify.backup <<EOF
+sudo tee /etc/cron.daily/cloudify-backup <<EOF
 logger '\''BEGIN RESTIC'\''
 export RESTIC_PASSWORD='\''${RESTIC_PASSWORD}'\''
 export RCLONE_TIMEOUT=50
-/usr/local/bin/resticfy All rbc >> /var/log/cloudify.backup.log  2>&1
+/usr/local/bin/resticfy All rbc >> /var/log/cloudify-backup.log  2>&1
 unset RESTIC_PASSWORD
 logger '\''END RESTIC'\''
 EOF
 
-#sudo chown root:root /etc/cron.daily/cloudify.backup
-sudo chmod 700 /etc/cron.daily/cloudify.backup
+#sudo chown root:root /etc/cron.daily/cloudify-backup
+sudo chmod 700 /etc/cron.daily/cloudify-backup
 PKG_DEBUG "Created Default Backup cron job."
 
